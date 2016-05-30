af54ef33b723725250919495e94b85b1ec68d925
Wilan Bigay <wilan.bigay@clickview.com.au>
2016-05-30 15:24:58 +1000

    add Client side dev environment

---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+
+/// <reference path="gulp/gulp.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="orchestrator/orchestrator.d.ts" />
+/// <reference path="q/Q.d.ts" />
+/// <reference path="systemjs/systemjs.d.ts" />
